0e81f9af914603ddac3d6cb15b51b3cae764b78e
NexGenUA <mr.rinus@gmail.com>
2020-02-17 17:11:22 +0200

    feat: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,36 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Object.prototype.toString.call(arr) !== "[object Array]")
+    throw new Error();
+
+  var result = [];
+
+  function idx(d) {
+    return [
+      "--discard-next",
+      "--discard-prev",
+      "--double-next",
+      "--double-prev"
+    ].indexOf(d);
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    if (idx(arr[i]) === 0) {
+      i++;
+      continue;
+    }
+    if (idx(arr[i]) === 1) {
+      result.pop();
+      continue;
+    }
+    if (idx(arr[i]) === 2) {
+      if (i + 1 < arr.length) result.push(arr[i + 1]);     
+      continue;
+    }
+    if (idx(arr[i]) === 3) {
+      if (i > 0) result.push(arr[i - 1]);
+      continue;
+    }
+    result.push(arr[i]);
+  }
+  return result;
 };
