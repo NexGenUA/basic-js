69b5a2421cb2eb08c9afd2962546b893f6400d0a
NexGenUA <mr.rinus@gmail.com>
2020-02-17 12:17:18 +0200

    feat: dream-team and what-season done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var coolSecretName = [];
+  if (!members) return false;
+  for (var i = 0; i <  members.length; i++) {
+    if (typeof members[i] === 'string') {
+      coolSecretName.push(members[i].trim()[0].toUpperCase())
+    }
+  }
+  if (!coolSecretName.length) return false;
+  return coolSecretName.sort().join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,14 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  var m = date.getMonth();
+
+  if (Object.keys(date).length > 0) {
+    throw new Error();
+  }
+
+  return /2|3|4/.test(m) 
+         ? 'spring' : /5|6|7/.test(m)
+         ? 'summer' : /8|9|10/.test(m)
+         ? 'autumn' : 'winter'
+ 
 };
