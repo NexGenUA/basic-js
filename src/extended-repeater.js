c2e916bca5f80156b3d620be138b78a50e549fb2
NexGenUA <mr.rinus@gmail.com>
2020-03-06 22:42:40 +0200

    feat: extended-repeat done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,5 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const { repeatTimes, separator  = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '' } = options;
+  str += (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  return (str + separator).repeat(repeatTimes - 1) + str;
 };
-  
