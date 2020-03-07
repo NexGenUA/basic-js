57c060f2a1d977ff1cfe4709dc061016f7daa201
NexGenUA <mr.rinus@gmail.com>
2020-03-07 13:46:29 +0200

    feat: vigenere-cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,70 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  
+  constructor(reverse = true) {
+    this.reverse = !reverse;
+  }
+
+  encrypt(str, codePhrase) {
+    
+    if (!str || !codePhrase) throw new Error;
+
+    let offset = 0;
+    let result = "";
+    
+    codePhrase = codePhrase.repeat(Math.ceil(str.length / codePhrase.length));
+
+    for (let i = 0; i < str.length; i++) {
+    
+      const char = this.getChar(str[i], codePhrase[i - offset], '+');
+    
+      if (!/[a-z]/ig.test(char)) offset++;
+    
+      result += char;
+    
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    result = this.reverse ? [...result].reverse().join('') : result;
+
+    return result;
+  }
+
+  decrypt(str, codePhrase) {
+
+    if (!str || !codePhrase) throw new Error;
+
+    let offset = 0;
+    let result = "";
+
+    codePhrase = codePhrase.repeat(Math.ceil(str.length / codePhrase.length));
+
+    for (let i = 0; i < str.length; i++) {
+      
+      const char = this.getChar(str[i], codePhrase[i - offset]);
+
+      if (!/[a-z]/ig.test(char)) offset++;
+
+      result += char;
+
     }
+
+    result = this.reverse ? [...result].reverse().join('') : result;
+
+    return result;
+  }
+
+  getChar(x, y, operator) {
+
+    const strChar = x.toUpperCase().codePointAt(0) - 65;
+    const codePhraseChar = y.toUpperCase().codePointAt(0) - 65;
+
+    if (strChar < 0 || strChar > 25) return x.toUpperCase();
+
+    let code = operator === '+' ? strChar + codePhraseChar : strChar - codePhraseChar;
+
+    code = code > 25 ? code - 26 : code < 0 ? code + 26 : code;
+    
+    return String.fromCodePoint(code + 65);
+  }
 }
 
 module.exports = VigenereCipheringMachine;
